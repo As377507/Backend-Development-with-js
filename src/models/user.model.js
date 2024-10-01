b4873bdfdb4277720b2b8f3e4003892e77072d87
Akash Sen <asen05897@gmail.com>
2024-10-01 18:37:23 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     username:{
-        type: String,
+        type: String ,
         required: true,
         unique: true,
         lowercase: true,
@@ -13,7 +13,7 @@ const userSchema = new Schema({
 
     },
     email:{
-        type: String,
+        type: String ,
         required: true,
         unique: true,
         lowercase: true,
@@ -60,7 +60,7 @@ const userSchema = new Schema({
 
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password,10)
+    this.password = await bcrypt.hash(this.password,10)
     next() 
 })
 
